0e24490311cf0250d18f523cd1915acc3d3f2113
manish-rdy <77088146+manish-rdy@users.noreply.github.com>
2024-12-07 17:44:12 +0530

    Create all_commands.sh

---
+++ Linux/all_commands.sh
@@ -0,0 +1,21 @@
+# Hello World Program in Bash Shell
+
+#echo $PATH
+# uname -r
+# dmesg | grep -i usb
+# udevadm info --query=path
+# udevadm monitor
+# lspci
+# lsblk
+# lscpu
+# lsmem --summary
+# free -g
+# lshw
+# ls -l /sbin/init
+# runlevel
+# systemctl get-default
+
+# To change the target
+# sudo systemctl set-default multi-user.target
+
+# sudo file /root/firefox.deb
